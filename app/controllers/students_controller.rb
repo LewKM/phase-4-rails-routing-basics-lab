ed85d0840750bb9a803ecb3cff53a75120410cc7
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-26 18:51:09 +0300

    Done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,20 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    # In the grades action, return JSON data representing a list of all students, ordered by grade from highest to lowest
+    def grades
+        students = Student.all.order(grade: :desc)
+        render json: students
+    end
+
+    # In the highest_grade action, return JSON data representing the one student with the highest grade
+    def highest_grade
+        student = Student.all.order(grade: :desc).first
+        render json: student
+    end
+end
+
